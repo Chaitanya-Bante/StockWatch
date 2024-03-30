b670ec4e40d2d5b79003dc9167f6b90906793819
Chaitanya-Bante <urs.chaitanyaa@gmail.com>
2024-03-30 10:21:54 +0530

    Can now fetch stocks names and changes

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ response = requests.get(url)
 
 soup = bs(response.text, 'html.parser')
 
-links = soup.find_all('a')
+stocks = soup.find_all('a', {'class':'NaLFgc'})
 
-for link in links:
-    print(link.get('href'))
+stock_names = []
+stock_change = []
+
+for stock in stocks:
+    stock_names = soup.find_all('div', {'class':'TwnKPb'})
+    stock_change = soup.find_all('div', {'class':'JwB6zf'})
+    
+for i in range(len(stocks)):
+        print(stock_names[i].string + ' ' + str(stock_change[i].string))
